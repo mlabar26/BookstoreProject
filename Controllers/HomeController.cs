1d481a8a02f1e2d2b01d41cef3ae9ede8cd2c5d2
Marcus La Bar <mlabar26@byu.edu>
2023-03-01 22:19:15 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using BookstoreProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookstoreProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Creates an instance of the context file
+        private BookstoreContext context { get; set; }
+
+        //Creates a constructor
+        public HomeController(BookstoreContext temp)
+        {
+            context = temp;
+        }
+
+        public IActionResult Index()
+        {
+            var BookstoreData = context.Books.ToList();
+
+            return View(BookstoreData);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,60 @@
+using BookstoreProject.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookstoreProject
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public IConfiguration Configuration { get; set; }
+
+        public Startup (IConfiguration temp)
+        {
+            Configuration = temp;
+        }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Allows MVC setup
+            services.AddControllersWithViews();
+
+            services.AddDbContext<BookstoreContext>(options =>
+            {
+                options.UseSqlite(Configuration["ConnectionStrings:BookstoreDBConnection"]);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            //Tells program to use files in wwwroot folder
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                //Follows controller first, then action, then ID
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
