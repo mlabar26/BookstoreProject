860abe212a079489c65ed56046165f3795b48735
Marcus La Bar <mlabar26@byu.edu>
2023-03-02 21:52:40 -0700

    Update

--- Infrastructure/PaginationTagHelper.cs
+++ Infrastructure/PaginationTagHelper.cs
@@ -29,7 +29,11 @@ namespace BookstoreProject.Infrastructure
         //Different than View Context
         public PageInfo PageModel { get; set; }
         public string PageAction { get; set; }
-        
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
         public override void Process(TagHelperContext thc, TagHelperOutput tho)
         {
             IUrlHelper uh = uhf.GetUrlHelper(vc);
@@ -41,6 +45,12 @@ namespace BookstoreProject.Infrastructure
                 TagBuilder tb = new TagBuilder("a");
 
                 tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+                if (PageClassesEnabled)
+                {
+                    tb.AddCssClass(PageClass);
+                    tb.AddCssClass(i == PageModel.CurrentPage
+                        ? PageClassSelected : PageClassNormal);
+                }
                 tb.InnerHtml.Append(i.ToString());
 
                 final.InnerHtml.AppendHtml(tb);
